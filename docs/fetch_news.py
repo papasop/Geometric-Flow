9973b056913a3281ea7fe41f8ea9f1f100458146
bai <bai@baideMacBook-Pro.local>
2026-05-13 21:18:37 +1200

    Add China robotics index

--- docs/fetch_news.py
+++ docs/fetch_news.py
@@ -112,6 +112,7 @@ OFFICIAL_SOURCES = [
     {"name": "China Prosperity · Moutai", "url": "https://news.google.com/rss/search?q=%22Kweichow%20Moutai%22%20OR%20%22Moutai%22%20OR%20%22600519%22&hl=en-US&gl=US&ceid=US:en"},
     {"name": "China Prosperity · Maifu", "url": "https://news.google.com/rss/search?q=%22Maifu%20Technology%22%20OR%20%22%E8%BF%88%E5%AF%8C%E6%97%B6%22%20OR%20%2202556.HK%22&hl=en-US&gl=US&ceid=US:en"},
     {"name": "China Precision · Components", "url": "https://news.google.com/rss/search?q=%22Impro%20Precision%22%20OR%20%22%E9%B9%B0%E6%99%AE%E7%B2%BE%E5%AF%86%22%20OR%20%2201286.HK%22%20OR%20%22Luxshare%20Precision%22%20OR%20%22%E7%AB%8B%E8%AE%AF%E7%B2%BE%E5%AF%86%22%20OR%20%22002475.SZ%22%20OR%20%22Tsugami%20China%22%20OR%20%22%E6%B4%A5%E4%B8%8A%E6%9C%BA%E5%BA%8A%E4%B8%AD%E5%9B%BD%22%20OR%20%2201651.HK%22&hl=en-US&gl=US&ceid=US:en"},
+    {"name": "China Robotics · Components", "url": "https://news.google.com/rss/search?q=%22JL%20MAG%22%20OR%20%22JLMAG%22%20OR%20%22%E9%87%91%E5%8A%9B%E6%B0%B8%E7%A3%81%22%20OR%20%22300748.SZ%22%20OR%20%2206680.HK%22%20OR%20%22Tsugami%20China%22%20OR%20%22%E6%B4%A5%E4%B8%8A%E6%9C%BA%E5%BA%8A%E4%B8%AD%E5%9B%BD%22%20OR%20%2201651.HK%22%20OR%20%22robotics%22%20OR%20%22%E6%9C%BA%E5%99%A8%E4%BA%BA%22&hl=en-US&gl=US&ceid=US:en"},
 ]
 
 # Backward-compatible alias for older callers/comments.
@@ -160,6 +161,13 @@ PORTFOLIO_KEYWORDS = {
         "tsugami china", "01651", "1651.hk",
         "中国精密", "精密制造", "精密铸件", "鹰普精密", "立讯精密",
         "津上机床中国", "津上机床", "数控机床", "高端制造",
+    ],
+    "china-robotics": [
+        "robotics", "industrial robot", "humanoid robot", "servo", "rare earth magnet",
+        "permanent magnet", "neodymium", "jl mag", "jlmag", "300748", "06680",
+        "tsugami china", "01651", "1651.hk", "cnc", "machine tools",
+        "中国机器人", "机器人", "工业机器人", "人形机器人", "稀土永磁", "钕铁硼",
+        "金力永磁", "津上机床中国", "津上机床", "数控机床",
     ],
     "biotech": [
         "biotech", "biotechnology", "pharma", "drug", "therapy", "clinical", "fda",
@@ -260,6 +268,7 @@ SOURCE_PORTFOLIO_MATCHES = {
     "China Prosperity · Moutai": ["china-leading", "brand"],
     "China Prosperity · Maifu": ["china-leading"],
     "China Precision · Components": ["robot-war"],
+    "China Robotics · Components": ["china-robotics"],
     "MP Materials IR": ["us-rare-earth"],
     "Rare Earth News · Industry": ["us-rare-earth"],
     "Rare Earth News · MP": ["us-rare-earth"],
