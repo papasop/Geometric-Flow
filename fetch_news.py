2320e3f67d5e9f5b81b39d1765de18841630a074
bai <bai@baideMacBook-Pro.local>
2026-05-14 17:41:45 +1200

    Remove China precision index

--- fetch_news.py
+++ fetch_news.py
@@ -111,7 +111,6 @@ OFFICIAL_SOURCES = [
     {"name": "China Prosperity · PDD", "url": "https://news.google.com/rss/search?q=%22PDD%20Holdings%22%20OR%20Pinduoduo%20OR%20Temu&hl=en-US&gl=US&ceid=US:en"},
     {"name": "China Prosperity · Moutai", "url": "https://news.google.com/rss/search?q=%22Kweichow%20Moutai%22%20OR%20%22Moutai%22%20OR%20%22600519%22&hl=en-US&gl=US&ceid=US:en"},
     {"name": "China Prosperity · Maifu", "url": "https://news.google.com/rss/search?q=%22Maifu%20Technology%22%20OR%20%22%E8%BF%88%E5%AF%8C%E6%97%B6%22%20OR%20%2202556.HK%22&hl=en-US&gl=US&ceid=US:en"},
-    {"name": "China Precision · Components", "url": "https://news.google.com/rss/search?q=%22Impro%20Precision%22%20OR%20%22%E9%B9%B0%E6%99%AE%E7%B2%BE%E5%AF%86%22%20OR%20%2201286.HK%22%20OR%20%22Luxshare%20Precision%22%20OR%20%22%E7%AB%8B%E8%AE%AF%E7%B2%BE%E5%AF%86%22%20OR%20%22002475.SZ%22%20OR%20%22Tsugami%20China%22%20OR%20%22%E6%B4%A5%E4%B8%8A%E6%9C%BA%E5%BA%8A%E4%B8%AD%E5%9B%BD%22%20OR%20%2201651.HK%22&hl=en-US&gl=US&ceid=US:en"},
     {"name": "China Robotics · Components", "url": "https://news.google.com/rss/search?q=%22JL%20MAG%22%20OR%20%22JLMAG%22%20OR%20%22%E9%87%91%E5%8A%9B%E6%B0%B8%E7%A3%81%22%20OR%20%22300748.SZ%22%20OR%20%2206680.HK%22%20OR%20%22Tsugami%20China%22%20OR%20%22%E6%B4%A5%E4%B8%8A%E6%9C%BA%E5%BA%8A%E4%B8%AD%E5%9B%BD%22%20OR%20%2201651.HK%22%20OR%20%22robotics%22%20OR%20%22%E6%9C%BA%E5%99%A8%E4%BA%BA%22&hl=en-US&gl=US&ceid=US:en"},
 ]
 
@@ -153,14 +152,6 @@ PORTFOLIO_KEYWORDS = {
         "pdd holdings", "pdd", "pinduoduo", "拼多多", "temu", "新拼姆",
         "kweichow moutai", "moutai", "600519", "贵州茅台", "茅台",
         "maifu technology", "maifushi", "maifu", "迈富时", "02556", "2556.hk", "2556",
-    ],
-    "robot-war": [
-        "precision manufacturing", "precision casting", "precision components", "cnc",
-        "machine tools", "high-end manufacturing", "impro precision", "impro",
-        "01286", "1286.hk", "luxshare precision", "luxshare", "002475",
-        "tsugami china", "01651", "1651.hk",
-        "中国精密", "精密制造", "精密铸件", "鹰普精密", "立讯精密",
-        "津上机床中国", "津上机床", "数控机床", "高端制造",
     ],
     "china-robotics": [
         "robotics", "industrial robot", "humanoid robot", "servo", "rare earth magnet",
@@ -267,7 +258,6 @@ SOURCE_PORTFOLIO_MATCHES = {
     "China Prosperity · PDD": ["china-leading", "brand"],
     "China Prosperity · Moutai": ["china-leading", "brand"],
     "China Prosperity · Maifu": ["china-leading"],
-    "China Precision · Components": ["robot-war"],
     "China Robotics · Components": ["china-robotics"],
     "MP Materials IR": ["us-rare-earth"],
     "Rare Earth News · Industry": ["us-rare-earth"],
