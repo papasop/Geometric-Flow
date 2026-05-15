a3c329ef4f4b98526982027eb908a8ba07b24404
bai <bai@baideMacBook-Pro.local>
2026-05-15 16:41:39 +1200

    Add Huawei Hubble portfolio

--- fetch_news.py
+++ fetch_news.py
@@ -99,6 +99,7 @@ OFFICIAL_SOURCES = [
     {"name": "Brand News · JNBY", "url": "https://news.google.com/rss/search?q=%22Jiangnan%20Buyi%22&hl=en-US&gl=US&ceid=US:en"},
     {"name": "Brand News · PDD", "url": "https://news.google.com/rss/search?q=%22PDD%20Holdings%22%20OR%20Pinduoduo%20OR%20Temu&hl=en-US&gl=US&ceid=US:en"},
     {"name": "China Robotics · Components", "url": "https://news.google.com/rss/search?q=%22Tsugami%20China%22%20OR%20%22%E6%B4%A5%E4%B8%8A%E6%9C%BA%E5%BA%8A%E4%B8%AD%E5%9B%BD%22%20OR%20%2201651.HK%22%20OR%20%22RoboSense%22%20OR%20%22%E9%80%9F%E8%85%BE%E8%81%9A%E5%88%9B%22%20OR%20%2202498.HK%22%20OR%20%22robotics%22%20OR%20%22%E6%9C%BA%E5%99%A8%E4%BA%BA%22&hl=en-US&gl=US&ceid=US:en"},
+    {"name": "Huawei Hubble · Components", "url": "https://news.google.com/rss/search?q=%22Huawei%20Hubble%22%20OR%20%22%E5%8D%8E%E4%B8%BA%E5%93%88%E5%8B%83%22%20OR%20%22Tianyue%20Advanced%22%20OR%20%223Peak%22%20OR%20%22Vanchip%22%20OR%20%22JoulWatt%22%20OR%20%22%E5%A4%A9%E5%B2%B3%E5%85%88%E8%BF%9B%22%20OR%20%22%E6%80%9D%E7%91%9E%E6%B5%A6%22%20OR%20%22%E5%94%AF%E6%8D%B7%E5%88%9B%E8%8A%AF%22%20OR%20%22%E6%9D%B0%E5%8D%8E%E7%89%B9%22&hl=en-US&gl=US&ceid=US:en"},
 ]
 
 # Backward-compatible alias for older callers/comments.
@@ -122,6 +123,17 @@ PORTFOLIO_KEYWORDS = {
         "lidar", "robotic vision", "cnc", "machine tools",
         "中国机器人", "机器人", "工业机器人", "人形机器人",
         "津上机床中国", "津上机床", "速腾聚创", "激光雷达", "机器人视觉", "数控机床",
+    ],
+    "huawei": [
+        "huawei", "hubble", "huawei hubble", "semiconductor", "sic", "silicon carbide",
+        "analog chip", "power management", "rf front-end", "optical chip", "ethernet phy",
+        "memory chip", "wireless charging", "connector", "chip testing",
+        "688234", "688809", "688536", "688629", "688153", "688498",
+        "688141", "688515", "688048", "688261", "688110", "688458",
+        "华为", "哈勃", "华为哈勃", "半导体", "碳化硅", "模拟芯片", "电源管理",
+        "射频前端", "光芯片", "以太网物理层", "存储芯片", "无线充电", "连接器", "芯片测试",
+        "天岳先进", "强一股份", "思瑞浦", "华丰科技", "唯捷创芯", "源杰科技",
+        "杰华特", "裕太微", "长光华芯", "东微半导", "东芯股份", "美芯晟",
     ],
     "biotech": [
         "biotech", "biotechnology", "pharma", "drug", "therapy", "clinical", "fda",
@@ -199,6 +211,7 @@ SOURCE_PORTFOLIO_MATCHES = {
     "Brand News · JNBY": ["brand"],
     "Brand News · PDD": ["brand"],
     "China Robotics · Components": ["china-robotics"],
+    "Huawei Hubble · Components": ["huawei"],
     "MP Materials IR": ["us-rare-earth"],
     "Rare Earth News · Industry": ["us-rare-earth"],
     "Rare Earth News · MP": ["us-rare-earth"],
